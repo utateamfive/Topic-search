3284d8972eb41a4fd995b6fc3c1cd05906ae7002
utateamfive <104228259+utateamfive@users.noreply.github.com>
2022-04-29 15:24:39 -0500

    Add files via upload

---
+++ com/teamfive/universitybazaarsystem/TopicRecycler.java
@@ -0,0 +1,42 @@
+package com.teamfive.universitybazaarsystem;
+
+public class TopicRecycler {
+    private String Topicname;
+    private String Topiccategory;
+    private String TopicAuthor;
+    private String Bodytext;
+
+    public String getTopicname() {
+        return Topicname;
+    }
+
+    public void setTopicname(String topicname) {
+        Topicname = topicname;
+    }
+
+    public String getTopiccategory() {
+        return Topiccategory;
+    }
+
+    public void setTopiccategory(String topiccategory) {
+        Topiccategory = topiccategory;
+    }
+
+    public String getTopicAuthor() {
+        return TopicAuthor;
+    }
+
+    public void setTopicAuthor(String topicAuthor) {
+        TopicAuthor = topicAuthor;
+    }
+
+    public String getBodytext() {
+        return Bodytext;
+    }
+
+    public void setBodytext(String bodytext) {
+        Bodytext = bodytext;
+    }
+
+
+}
